0754d267628532ec7e70f502d50bb0956642ee58
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-12 11:15:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ require 'pry'
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
-def directors_totals(nds)
+def directors_totals()
 
 
 
@@ -15,7 +15,7 @@ end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
+def gross_for_director()
 
 
 
