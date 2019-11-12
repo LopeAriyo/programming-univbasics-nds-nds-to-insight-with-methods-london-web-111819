a2e07cee177297c2cf9e5c7382bc6348802c7dbe
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-12 11:59:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,7 +13,7 @@ list_of_directors_totals = {}
 
   nds.each do |director|
     director.each do |key, value|
-      if key == :name 
+      if key == :name
         value.each do
           
         #list_of_directors_totals[name] = gross_for_director(value)
